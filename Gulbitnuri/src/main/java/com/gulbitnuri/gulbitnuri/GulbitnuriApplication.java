2c2f464f793799efe551cc8fb0fe94d8fb49721e
BlueHarmel <dldjdtjr@naver.com>
2024-07-31 23:56:21 +0900

    first Commit

---
+++ Gulbitnuri/src/main/java/com/gulbitnuri/gulbitnuri/GulbitnuriApplication.java
@@ -0,0 +1,13 @@
+package com.gulbitnuri.gulbitnuri;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GulbitnuriApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GulbitnuriApplication.class, args);
+    }
+
+}
---
+++ Gulbitnuri/src/test/java/com/gulbitnuri/gulbitnuri/GulbitnuriApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gulbitnuri.gulbitnuri;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GulbitnuriApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
